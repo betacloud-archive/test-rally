a450bf64d8a9ed16e7c4a211876f0ef25710a877
Christian Berendt <berendt@betacloud-solutions.de>
2019-04-24 23:09:44 +0200

    Prepare minio in prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,3 +6,8 @@ rally env create --name betacloud --spec betacloud.yml 2>&1 > /dev/null
 rally env check
 
 rally deployment check
+
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc
+
+./mc config host add betacloud https://files.betacloud.de $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
--- scripts/push.sh
+++ scripts/push.sh
@@ -2,11 +2,6 @@
 
 uuid=$(rally task list --status finished --uuids-only | head -n 1)
 
-wget https://dl.min.io/client/mc/release/linux-amd64/mc
-chmod +x mc
-
-./mc config host add betacloud https://files.betacloud.de $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
-
 ./mc mb --ignore-existing betacloud/travis-test-rally/
 ./mc mb --ignore-existing betacloud/travis-test-rally/latest/
 ./mc policy public betacloud/travis-test-rally/latest/
